a6f6bb6da8fa7f960e133776b2ac2270cdae8deb
Ron Heller <rgheller@gmail.com>
2021-06-30 21:36:39 +0200

    v2

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        VStack {
+            
         Text("Hello, world!")
             .padding()
+
+            Text("Hello, world 2!")
+                .padding()
+        }
     }
 }
 
