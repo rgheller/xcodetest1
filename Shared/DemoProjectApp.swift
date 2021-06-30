173084182dac060e5c359a29d79248ed927d9289
Ron Heller <rgheller@gmail.com>
2021-06-30 21:14:39 +0200

    Initial Commit

---
+++ Shared/DemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectApp.swift
+//  Shared
+//
+//  Created by Ron Heller on 30/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
